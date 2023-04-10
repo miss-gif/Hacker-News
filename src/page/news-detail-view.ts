5a938bcb9766f60d67f6db48b1d069eeaf7fc654
miss <ddc.akari@gmail.com>
2023-04-10 22:56:47 +0900

    async, await 사용

--- src/page/news-detail-view.ts
+++ src/page/news-detail-view.ts
@@ -1,7 +1,6 @@
-import View from "../core/view";
-import { NewsDetailApi } from "../core/api";
-import { NewsComment, NewsStore } from "../types";
-import { CONTENT_URL } from "../config";
+import View from '../core/view';
+import { NewsDetailApi } from '../core/api';
+import { NewsComment, NewsDetail, NewsStore } from '../types';
 
 const template = `
 <div class="bg-gray-600 min-h-screen pb-8">
@@ -34,25 +33,26 @@ export default class NewsDetailView extends View {
   private store: NewsStore;
 
   constructor(containerId: string, store: NewsStore) {
-    super(containerId, template);
+    super(containerId, template);  
     this.store = store;
   }
 
-  render = (id: string): void => {
-    const api = new NewsDetailApi(CONTENT_URL.replace("@id", id));
-    const { title, content, comments } = api.getData();
+  render = async (id: string): Promise<void> => {
+    const api = new NewsDetailApi(id);
+    
+    const { title, content, comments } = await api.getData();
 
     this.store.makeRead(Number(id));
-    this.setTemplateData("currentPage", this.store.currentPage.toString());
-    this.setTemplateData("title", title);
-    this.setTemplateData("content", content);
-    this.setTemplateData("comments", this.makeComment(comments));
+    this.setTemplateData('currentPage', this.store.currentPage.toString());
+    this.setTemplateData('title', title);
+    this.setTemplateData('content', content);
+    this.setTemplateData('comments', this.makeComment(comments));
 
     this.updateView();
-  };
+  }
 
   private makeComment(comments: NewsComment[]): string {
-    for (let i = 0; i < comments.length; i++) {
+    for(let i = 0; i < comments.length; i++) {
       const comment: NewsComment = comments[i];
 
       this.addHtml(`
@@ -64,12 +64,12 @@ export default class NewsDetailView extends View {
           <p class="text-gray-700">${comment.content}</p>
         </div>      
       `);
-
+  
       if (comment.comments.length > 0) {
         this.addHtml(this.makeComment(comment.comments));
       }
     }
-
+  
     return this.getHtml();
   }
 }
